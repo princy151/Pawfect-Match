d3aafda098c5c598e3ca26730264fa99423c154d
princy151 <agrawalprincy151@gmail.com>
2025-06-18 18:29:51 +0545

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,26 @@ import ProfilePage from './pages/adopter/profile';
 import CartPage from './pages/adopter/cart';
 import AHomePage from './pages/adopter/home';
 import ShelterHomePage from './pages/shelter/shelterhome';
+import ShopPage from './pages/shelter/sheltershop';
+import PetDetails from './pages/shelter/petdetails';
+import SAboutUsPage from './pages/shelter/shelteraboutus';
+import SAdoptionForm from './pages/shelter/shelterform';
+import SLogin from './pages/shelter/shelterlogin';
+import SRegister from './pages/shelter/sheltersignup';
+import LandingPage from './pages/landingpage';
+import APetsPage from './pages/adopter/petspage';
+import PetsPage from './pages/adopter/petspage';
+import ASecondPetPage from './pages/adopter/petsdata';
+import AMorePets from './pages/adopter/morepets';
 
 const router = createBrowserRouter([
+
+  {
+    path: '/landingpage',
+    element: < LandingPage/>,
+  },
+
+
   // Adopter Routes
   {
     path: '/adopterhome',
@@ -45,10 +63,52 @@ const router = createBrowserRouter([
     path: '/adoptercart',
     element: < CartPage/>,
   },
+  {
+    path: '/adopterpets',
+    element: < PetsPage/>,
+  },
+  {
+    path: '/adopterpetstwo',
+    element: < ASecondPetPage/>,
+  },
+  {
+    path: '/adoptermorepets',
+    element: < AMorePets/>,
+  },
+
+
+
+
+
+
   //Shelter Routes
   {
     path: '/shelterhome',
     element: < ShelterHomePage/>,
+  },
+  {
+    path: '/sheltershop',
+    element: < ShopPage/>,
+  },
+  {
+    path: '/shelterpets',
+    element: < PetDetails/>,
+  },
+  {
+    path: '/shelteraboutus',
+    element: < SAboutUsPage/>,
+  },
+  {
+    path: '/shelterform',
+    element: < SAdoptionForm/>,
+  },
+  {
+    path: '/shelterlogin',
+    element: < SLogin/>,
+  },
+  {
+    path: '/sheltersignup',
+    element: < SRegister/>,
   },
 ]);
 
